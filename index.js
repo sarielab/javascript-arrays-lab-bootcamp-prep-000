e8c9477eeb5f8e780d760209dd45d869d289faa9
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-08 16:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,29 +2,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 var destructivelyAppendKitten = (el) => kittens.push(el)
-var destructivelyPrependKitten = el => arr.unshift(el)
-var destructivelyRemoveLastKitten = () => arr.pop()
-var destructivelyRemoveFirstKitten = () => arr.shift()
-var appendKitten = (el) => arr.concat(el)
-var prependKitten = (el) => [el, ...kitten]
-
-
-describe('Arrays', function() {
-  describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-  describe('removeFirstKitten()', function() {
-    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-})
+var destructivelyPrependKitten = el => kittens.unshift(el)
+var destructivelyRemoveLastKitten = () => kittens.pop()
+var destructivelyRemoveFirstKitten = () => kittens.shift()
+var appendKitten = (el) => kittens.concat(el)
+var prependKitten = (el) => [el, ...kittens]
+var removeLastKitten = () => kittens.slice(0, -1)
+var removeFirstKitten = () => kittens.slice(1)
